f6c268cbc713e758fdaa79d116201afec64af388
iko <ilyakooo0@gmail.com>
2021-12-04 19:33:13 +0300

    Added useClosureCompiler

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
 , closurecompiler ? pkgs.closurecompiler
 , zopfli ? pkgs.zopfli
 , useZopfli ? true
+, useClosureCompiler ? true
 , createHTML ? true
 , allJsPath ? "/bin/*/all.js"
 }:
@@ -18,8 +19,12 @@ pkgs.runCommand "${input.name}-optimized"
   chmod +w -R $out
 
   rm $out${allJsPath} $out${input}${allJsPath}.externs $out/all.js $out/all.js.externs || true
-
-  ${closurecompiler}/bin/closure-compiler --compilation_level ADVANCED --jscomp_off=checkVars --warning_level QUIET --js ${input}${allJsPath} --externs ${input}${allJsPath}.externs --js_output_file $out/all.js
+  
+  ${
+    if useClosureCompiler
+      then "${closurecompiler}/bin/closure-compiler --compilation_level ADVANCED --jscomp_off=checkVars --warning_level QUIET --js ${input}${allJsPath} --externs ${input}${allJsPath}.externs --js_output_file $out/all.js"
+      else ""
+  }
 
   ${ if useZopfli then "${zopfli}/bin/zopfli $out/**/*.{js,css,json,html}" else "" }
 ''
